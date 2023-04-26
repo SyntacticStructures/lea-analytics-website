68bcb041fcbc12f13fd241757e13fa23d23a9513
Computer <computer@Computers-Laptop.local>
2023-04-26 09:28:38 -0700

    Add form submit modals and fix logo styling

--- js/form.js
+++ js/form.js
@@ -3,7 +3,7 @@ const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
    e.preventDefault()
-   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
+   $('#form-submit-modal').modal('show')
+   fetch(scriptURL, { method: 'POST', body: new FormData(form)});
+   form.reset();
 })
